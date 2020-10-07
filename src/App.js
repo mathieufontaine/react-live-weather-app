1098563259eb06056146dce73a4c2f8a1a63be27
MATHIEU FONTAINE <mathieufontaine@mathieus-macbook-pro.home>
2020-10-07 18:57:44 +0200

    Initial workflow setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/layout/Header";
+import Navbar from "./components/layout/Navbar";
+import Card from "./components/layout/Card";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Navbar />
+      <div className="container">
+        <Card />
+      </div>
+      <Footer />
+    </>
   );
 }
 
